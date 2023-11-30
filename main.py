9183cf58c5d356fc38cfba97c8505c939c67e71c
Strong Real Nine <63622924+rkdwlsrn307@users.noreply.github.com>
2023-11-30 20:14:59 +0900

    11/28

--- main.py
+++ main.py
@@ -4,35 +4,14 @@ from Menu import *
 
 def main(page: Page):
     page.title = "Jobports"
-<<<<<<< HEAD
     page.window_width = 480
     page.window_height = 800
-=======
-<<<<<<< HEAD
-    page.window_width = 480
-    page.window_height = 800
-=======
-<<<<<<< HEAD
-    page.window_width = 480
-    page.window_height = 800
-=======
-
->>>>>>> f981f4eba74a823f44575e275ef8f826b2634121
->>>>>>> cd361d3f67c9f3a6aa401f5747a538bf056b1434
->>>>>>> fdc43fdbe42b30d37db101fba99d4b7e29cee5de
     logo = Image(
             src = f"/잡포츠.png",
             width = 160,
             height = 80,
             fit = ImageFit.CONTAIN,)
     
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> cd361d3f67c9f3a6aa401f5747a538bf056b1434
->>>>>>> fdc43fdbe42b30d37db101fba99d4b7e29cee5de
     def change_page(e):
         my_index = e.control.selected_index
 
@@ -90,25 +69,6 @@ def main(page: Page):
                 ],
             ), 
     }
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
-    page.navigation_bar = NavigationBar(
-        destinations = [
-            NavigationDestination(icon = icons.EXPLORE, label="Explore"),
-            NavigationDestination(icon = icons.COMMUTE, label="Commute"),
-            NavigationDestination(
-                icon=icons.BOOKMARK_BORDER,
-                selected_icon = icons.BOOKMARK,
-                label = "Explore",
-            ),
-        ]
-    )
->>>>>>> f981f4eba74a823f44575e275ef8f826b2634121
->>>>>>> cd361d3f67c9f3a6aa401f5747a538bf056b1434
->>>>>>> fdc43fdbe42b30d37db101fba99d4b7e29cee5de
 
     def route_change(route):
         page.views.clear()
@@ -116,24 +76,9 @@ def main(page: Page):
             pages[page.route]
         )
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> cd361d3f67c9f3a6aa401f5747a538bf056b1434
->>>>>>> fdc43fdbe42b30d37db101fba99d4b7e29cee5de
     
     page.on_route_change = route_change
     page.go(page.route)
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> f981f4eba74a823f44575e275ef8f826b2634121
->>>>>>> cd361d3f67c9f3a6aa401f5747a538bf056b1434
->>>>>>> fdc43fdbe42b30d37db101fba99d4b7e29cee5de
 
 app(target = main, view = AppView.FLET_APP, assets_dir = "home_page")
